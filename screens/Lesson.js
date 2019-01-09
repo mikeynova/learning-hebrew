0047ceb24c3df36896e028d312f2de95f8769de1
Michael Terranova <newfoundland7@gmail.com>
2019-01-08 16:03:28 -0800

    Load pages. Play sound bites

--- screens/Lesson.js
+++ screens/Lesson.js
@@ -1,7 +1,10 @@
 import React, { PureComponent } from 'react'
-import { View, Text, Button, AsyncStorage, StyleSheet } from 'react-native'
+import { View, Text, Button, AsyncStorage, StyleSheet, TouchableOpacity, Dimensions, SafeAreaView } from 'react-native'
 import { Query } from 'react-apollo'
+import { Audio } from 'expo'
+import { Feather } from '@expo/vector-icons'
 import gql from "graphql-tag"
+const { width } = Dimensions.get('screen')
 
 class Lesson extends PureComponent {
   static navigationOptions = ({ navigation }) => ({
@@ -11,16 +14,101 @@ class Lesson extends PureComponent {
       </View>
     )
   })
+  constructor (props) {
+    super(props)
+    this.playbackInstance = null
+  }
+
+  componentDidMount () {
+    this._loadNewPlaybackInstance(false)
+  }
+
+  async _loadNewPlaybackInstance(playing) {
+    if (this.playbackInstance != null) {
+      await this.playbackInstance.unloadAsync()
+      this.playbackInstance.setOnPlaybackStatusUpdate(null)
+      this.playbackInstance = null
+    }
+
+    const source = { uri: 'https://ia802508.us.archive.org/5/items/testmp3testfile/mpthreetest.mp3' }
+    const initialStatus = {
+      shouldPlay: playing,
+    }
+
+    const { sound, status } = await Audio.Sound.createAsync(
+      source,
+      initialStatus,
+      this.onPlaybackStatusUpdate
+    )
+    this.playbackInstance = sound
+  }
+
+  onPlaybackStatusUpdate = (status) => {
+    if (status.didJustFinish) {
+      this.playbackInstance.setPositionAsync(0)
+    }
+  }
+
+  handlePress = (lesson, pageNumber) => {
+    pageNumber += 1
+    this.props.navigation.push('Lesson', { lesson, pageNumber })
+  }
+
+  handleEndLesson = () => {
+    this.props.navigation.navigate('Groups')
+  }
+
+  handlePlay = () => {
+    this.playbackInstance.playAsync()
+  }
 
   render () {
-    const { id } = this.props.navigation.state.params.lesson
+    const { lesson, pageNumber } = this.props.navigation.state.params
     return (
-      <Query query={LessonQuery} variables={{ id }}>
+      <Query query={PageQuery} variables={{ id: lesson.id, pageNumber }}>
         {({ loading, error, data }) => {
           if (loading) return <Text>Loading</Text>
-          if (error) return <Text>{`Error!: ${error}`}</Text>
+          if (error) return <Text>No pages for this lesson</Text>
+          const parsed = JSON.parse(data.page.data)
+          if (parsed.snippets) {
+            const snippets = parsed.snippets
+            return (
+              <View style={{ flex: 1, justifyContent: 'center', flexDirection: 'column' }}>
+                  {snippets.map((snippet, i) => {
+                    return (
+                      <View key={i} style={{ alignItems: 'center', flexDirection: 'row', justifyContent: 'space-between', margin: 15 }}>
+                        <Text>{snippet.english}</Text>
+                        <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+                          <TouchableOpacity style={{ marginRight: 5 }} onPress={() => this.handlePlay()}>
+                            <Feather  name='play'  />
+                          </TouchableOpacity>
+                          <View style={{ flexDirection: 'column' }}>
+                            <Text style={{ fontSize: 18 }}>{snippet.hebrew}</Text>
+                            <Text style={{ fontSize: 10 }}>{snippet.phoenetics}</Text>
+                          </View>
+                        </View>
+                      </View>
+                    )
+                  })}
+                <SafeAreaView style={{ position: 'absolute', bottom: 0, }}>
+                  <TouchableOpacity style={{ backgroundColor: 'red', width, height: 44, justifyContent: 'center' }} onPress={() => this.handleEndLesson()}>
+                    <Text style={{ textAlign: 'center', color: '#fff' }}>Finish Lesson</Text>
+                  </TouchableOpacity>
+                </SafeAreaView>
+              </View>
+            )
+          }
           return (
-            <Text>Hey</Text>
+            <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+              <View >
+                <Text>{parsed.english || parsed.hebrew}</Text> 
+              </View>
+              <SafeAreaView style={{ position: 'absolute', bottom: 0, }}>
+                <TouchableOpacity style={{  backgroundColor: 'red', width, height: 44, justifyContent: 'center' }} onPress={() => this.handlePress(lesson, pageNumber)}>
+                  <Text style={{ textAlign: 'center', color: '#fff' }}>Continue</Text>
+                </TouchableOpacity>
+              </SafeAreaView>
+            </View>
           )
         }}
       </Query>
@@ -46,6 +134,15 @@ const LessonQuery = gql`
         data
         pageNumber
       }
+    }
+  }
+`
+const PageQuery = gql`
+  query PageQuery($id: ID!, $pageNumber: Int!) {
+    page(id: $id, pageNumber: $pageNumber) {
+      id
+      pageNumber
+      data
     }
   }
 `
